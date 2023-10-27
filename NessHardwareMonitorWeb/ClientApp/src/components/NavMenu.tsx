6bd5f56807494da6a556c883f46bfc6fba9ec1e3
John Ness <j.ness93@gmail.com>
2023-10-26 23:38:31 -0400

    Add dark mode toggle

--- NessHardwareMonitorWeb/ClientApp/src/components/NavMenu.tsx
+++ NessHardwareMonitorWeb/ClientApp/src/components/NavMenu.tsx
@@ -7,12 +7,15 @@ import {
   NavItem,
   NavLink,
 } from "reactstrap";
+import { useColorMode, Button } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import "./NavMenu.css";
 
 const NavMenu = () => {
   const [collapsed, setCollapsed] = useState(true);
   const toggleNavbar = () => setCollapsed(!collapsed);
+  const { colorMode, toggleColorMode } = useColorMode();
+  const textClass = colorMode === "light" ? "text-dark" : "text-light";
 
   return (
     <header>
@@ -21,7 +24,7 @@ const NavMenu = () => {
         container
         light
       >
-        <NavbarBrand tag={Link} to="/">
+        <NavbarBrand tag={Link} className={textClass} to="/">
           Hardware Monitor
         </NavbarBrand>
         <NavbarToggler onClick={toggleNavbar} className="mr-2" />
@@ -32,17 +35,20 @@ const NavMenu = () => {
         >
           <ul className="navbar-nav flex-grow">
             <NavItem>
-              <NavLink tag={Link} className="text-dark" to="/">
+              <NavLink tag={Link} className={textClass} to="/">
                 Home
               </NavLink>
             </NavItem>
             <NavItem>
-              <NavLink tag={Link} className="text-dark" to="/monitor">
+              <NavLink tag={Link} className={textClass} to="/monitor">
                 Monitor
               </NavLink>
             </NavItem>
           </ul>
         </Collapse>
+        <Button onClick={toggleColorMode}>
+          Toggle {colorMode === "light" ? "Dark" : "Light"}
+        </Button>
       </Navbar>
     </header>
   );
